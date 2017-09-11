2ba1f0afcb7b0eddf15f66f4bd94b5b6918fe600
Luke Siemens <lukesiemens@gmail.com>
2017-09-11 15:21:37 +0000

    added a function so host and path weren ot hardcoded

---
+++ getAndPrintHTML2.js
@@ -0,0 +1,43 @@
+var https = require('https');
+
+//function to retrieve host and path(not hardcoded)
+function getAndPrintHTML (options) {
+	var options = {
+		host: "",
+		path: ""
+	}
+
+	options.host = requestOptions.host
+    options.path =  requestOptions.path
+  
+
+
+//GET Data from source 
+    https.get(requestOptions,function (response) {
+
+//explains how to encode incoming data
+	  response.setEncoding('utf8');
+
+//stores incoming data into a variable 
+		var collectedData = "";
+		response.on('data', function (data) {
+			collectedData += data;
+
+			console.log(collectedData);
+
+		});
+//callback is invoked when all of the data has been receiceved
+
+		response.on('end', function (){
+			console.log('Response stream complete.');
+		});
+	});
+
+
+}
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+}
+
+getAndPrintHTML();
