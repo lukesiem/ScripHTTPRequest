3196a3ac0b29613df8618676cf947d1f5c1950a3
Luke Siemens <lukesiemens@gmail.com>
2017-09-11 12:25:12 +0000

    wrote funks to get and print chunks

---
+++ getAndPrintChunks.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks() {
+
+	var requestOptions = {
+		host: 'sytantris.github.io',
+		path: '/http-examples/step1.html'
+	};
+
+	https.get(requestOptions, function (response) {
+
+		response.setEncoding('utf8');
+
+		response.on('data', function (data) {
+			console.log('Chunk Recieved Length:', data.length);
+			console.log(data + "\n");
+		});
+
+		response.on('end', function (){
+			console.log('Response stream complete.');
+		});
+	});
+
+
+
+
+}
+
+getAndPrintHTMLChunks();
